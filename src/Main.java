80985baa86b6be79ada5d0b42a4b04e01d149da0
Muhammed Ramees A <97622597+Ramees303@users.noreply.github.com>
2024-01-15 16:34:58 +0530

    xvfbdfbfdb

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+
+public class Main {
+
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("haiiiiiiii");
+        System.out.println(124);
+
+
+        Service sr=new Service();
+
+        int k=sr.calling();
+
+        System.out.println(k);
+
+        int i=1;
+        int j=9;
+
+        System.out.println();
+
+        System.out.println(i+j);
+    }
+}
